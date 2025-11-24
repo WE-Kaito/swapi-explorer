bbe57ecfe976084bf43d363612ce15ccba9007bf
Kaito <e.weissenrieder94@gmail.com>
2025-11-24 06:00:21 +0100

    feat: add star wars logo to Header component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,9 +1,11 @@
 import { PropsWithChildren } from "react";
+import Image from "next/image";
 
 export function Header({ children }: PropsWithChildren) {
-    return (
-        <header className="relative z-10 w-full py-1 flex justify-center bg-background/80 backdrop-blur-sm border-b border-foreground/10">
-            {children}
-        </header>
-    );
+  return (
+    <header className="relative z-10 w-full py-1 flex justify-center bg-background/80 backdrop-blur-sm border-b border-foreground/10">
+      <Image src="/assets/logo.svg" alt="STAR WARS" width={48} height={48} className={"absolute left-4 sm:left-8"} />
+      {children}
+    </header>
+  );
 }
